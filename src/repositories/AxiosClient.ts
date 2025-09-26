1a850ef8570725c816fae92d56e3bed5495167a3
Ajith Kumar <56600154+ajithkumarg23@users.noreply.github.com>
2025-09-26 21:24:37 +0530

    Update AxiosClient.ts

--- src/repositories/AxiosClient.ts
+++ src/repositories/AxiosClient.ts
@@ -9,7 +9,7 @@ type Param = Data | Array<unknown>;
 type ResponseData<T> = { data: T };
 
 export const register = async () => {
-  axios.defaults.baseURL = 'http://localhost:8080/';
+  axios.defaults.baseURL = 'http://140.245.209.64/';
   axios.defaults.headers.post['Content-Type'] = 'application/json';
 };
 
