8e13860147c956573fb6f0a278a6b669a5b91723
Ajith Kumar <56600154+ajithkumarg23@users.noreply.github.com>
2025-09-30 20:50:16 +0530

    Attendance calendar

--- src/components/Staff/StaffAttendanceTab.tsx
+++ src/components/Staff/StaffAttendanceTab.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import '../../utils/attendanceCalendar.css'
 
-const StaffAttendanceTab: React.FC = () => {
+// Define types for tileClassName properties
+interface TileClassNameProps {
+  date: Date;
+  view: string;
+}
+
+export default function StaffAttendanceTab() {
+  const [attendance] = useState<{ [key: string]: string }>({
+    '2025-09-01': 'present',
+    '2025-09-02': 'absent',
+    '2025-09-04': 'present',
+    '2025-09-06': 'absent',
+  });
+
+  // function to add custom class to each tile
+  const tileClassName = ({ date, view }: TileClassNameProps): string | null => {
+    if (view === 'month') {
+      // Local date string in India timezone
+      const dateStr = date.toLocaleDateString('en-CA'); // ✅ keeps IST
+
+      if (attendance[dateStr] === 'present') {
+        return 'present-date';
+      }
+      if (attendance[dateStr] === 'absent') {
+        return 'absent-date';
+      }
+    }
+    return null;
+  };
+
   return (
     <Box>
-      <Typography variant="h5">Attendance Content</Typography>
+      <Calendar tileClassName={tileClassName} next2Label={null} prev2Label={null} />
     </Box>
   );
-};
-
-export default StaffAttendanceTab;
+}
